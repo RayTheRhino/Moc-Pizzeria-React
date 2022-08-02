9711b911f8ee0b7e38d21c58d717ebd42c4ca8e1
Smiles <chenmay1696@gmail.com>
2022-08-02 16:57:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Menu from "./pages/Menu";
+// import About from "./pages/About";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+
+
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar/>
-      <Routes>
-        <Route path='/'exact component={Home}/>
-      </Routes>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element ={<Home/>} />
+          <Route path= "/menu" element ={<Menu/>} />
+          {/* <Route path= "/about" element ={<About/>} /> */}
+        </Routes>
       </Router>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/Home.css'
 
 function Home() {
     return (
-    <div>Home</div>
+    <div className="Home">
+        <div className='headerContainer'>
+            <h1> Pedros Pizzeria </h1>
+            <p> PIZZA TO FIT ANY TASTE </p>
+            <Link to="/menu">
+            <button> ORDER NOW</button>
+            </Link>
+        </div>
+    </div>
     )
 }
 
