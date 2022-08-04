2cb62a52a37a8ca80ab1e70b7ee96e312d141001
Smiles <chenmay1696@gmail.com>
2022-08-04 09:58:30 +0300

    updated footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from "./pages/Menu";
-// import About from "./pages/About";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
 
 
@@ -14,8 +16,10 @@ function App() {
         <Routes>
           <Route path="/" element ={<Home/>} />
           <Route path= "/menu" element ={<Menu/>} />
-          {/* <Route path= "/about" element ={<About/>} /> */}
+          <Route path= "/about" element ={<About/>} />
+          <Route path= "/contact" element ={<Contact/>} />
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
